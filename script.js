eca669e7fc75e1ff1b9cb28e113145529f00b11e
el hiho <hihi.cod@gmail.com>
2021-02-08 23:04:37 +0100

    prueba-error

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const fetchData = async() => {
     try {
-        const res = await fetch('./json/api.json')
+        const res = await fetch('api.json')
         const data = await res.json()
             // console.log(data)
         pintarProductos(data)
