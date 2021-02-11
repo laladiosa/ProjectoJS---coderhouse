d1274151e73ab1e1a3527b03b55c6a71403313f6
el hiho <hihi.cod@gmail.com>
2021-02-12 00:30:08 +0100

    Checkout

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let carrito = {}
     /******************EJEMPLO STORAGE************** */
-document.addEventListener("DOMContentLoaded", e => {
+document.addEventListener('DOMContentLoaded', e => {
     fetchData()
     if (localStorage.getItem('carrito')) {
         carrito = JSON.parse(localStorage.getItem('carrito'))
