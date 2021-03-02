175d8defcb85b430948b41972a48daf977420ada
el hiho <hihi.cod@gmail.com>
2021-03-03 00:35:02 +0100

    acomodando json

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ $(function() {
 
     const fetchData = async() => {
         try {
-            const res = await fetch('/json/api.json')
+            const res = await fetch('api.json')
             const data = await res.json()
                 // console.log(data)
             pintarProductos(data)
